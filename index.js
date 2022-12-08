ecc3af96ce723f9a3efdc7bc97f2e912512b7382
JesseKuntz <jessekuntz96@gmail.com>
2022-12-07 23:22:30 -0500

    refactor: space statements

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function getScrollbarWidth() {
   const outer = document.createElement('div');
+
   outer.style.visibility = 'hidden';
   outer.style.overflow = 'scroll';
   outer.style.msOverflowStyle = 'scrollbar';
+
   document.body.appendChild(outer);
+
   const inner = document.createElement('div');
+
   outer.appendChild(inner);
+
   const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
   outer.parentNode.removeChild(outer);
+
   return scrollbarWidth;
 }
 
